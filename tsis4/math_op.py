1e2735fd7e18f1b8191e13899ef741e94a39d020
Dias <kadirov.dias@mail.ru>
2024-02-10 21:36:17 +0600

    math

---
+++ tsis4/math_op.py
@@ -0,0 +1,13 @@
+import math
+
+degree = float(input('Input degree: '))
+print(math.radians(degree))
+
+height = float(input('Height: '))
+a = float(input('Base, first value: '))
+b = float(input('Base, second value: '))
+print(((a+b)/2) * height)
+
+sides = int(input('Input number of sides: '))
+l = float(input('Input legnth of side: '))
+print((1 / 4) * sides * l ** 2 * math.tan(math.pi / sides))
