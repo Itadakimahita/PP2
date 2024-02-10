b2c12237a13df557e3b8182422c75fe72b178b37
Dias <kadirov.dias@mail.ru>
2024-02-10 23:14:39 +0600

    date_parsing

--- tsis4/math_op.py
+++ tsis4/math_op.py
@@ -11,3 +11,7 @@ print(((a+b)/2) * height)
 sides = int(input('Input number of sides: '))
 l = float(input('Input legnth of side: '))
 print((1 / 4) * sides * l ** 2 * math.tan(math.pi / sides))
+
+h = int(input('Height: '))
+base = int(input('Base: '))
+print(h*base)
